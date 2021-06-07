133223297e1735c1767bc3e7c5830f9c41cb4cb3
eibens <l.eibensteiner@gmail.com>
2021-06-07 10:36:25 +0200

    :hammer: build: add build script

---
+++ build.sh
@@ -0,0 +1,31 @@
+COV_DIR=".cov"
+COV_FILE="mod.lcov"
+ALLOW_READ="/tmp,mockcli.ts"
+ALLOW_WRITE="/tmp"
+ALLOW_RUN="deno"
+
+# Run tests and generate coverage profile.
+deno test \
+--allow-read="$ALLOW_READ" \
+--allow-run="$ALLOW_RUN" \
+--allow-write="$ALLOW_WRITE" \
+--coverage="$COV_DIR"
+
+# Print coverage info to stdout.
+deno coverage \
+--unstable \
+"$COV_DIR"
+
+# Write coverage report to file.
+deno coverage \
+--unstable \
+--lcov \
+"$COV_DIR" \
+> "$COV_FILE"
+
+# Delete coverage profile.
+rm -rf "$COV_DIR"
+
+# Delete PWD from file names in coverage report.
+# Use @ as separator, so that we can use / unescaped.
+sed -i "s@SF:${PWD}/@SF:@" "$COV_FILE"
